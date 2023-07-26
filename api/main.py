ce7ccf6c641de67870ad5681e780db2e339b0c2f
maestroi <meastro@protonmail.com>
2023-07-26 22:56:25 +0200

    Add csv endpoint

--- api/main.py
+++ api/main.py
@@ -8,6 +8,9 @@ import uvicorn
 import os
 import mysql.connector
 from mysql.connector.pooling import MySQLConnectionPool
+import csv
+from starlette.responses import Response
+from fastapi.responses import StreamingResponse
 
 # Set up logging
 logging.basicConfig(
@@ -100,6 +103,66 @@ async def get_system_states(network: str = 'mainnet', epoch: int = None):
     except mysql.connector.Error as err:
         logging.error(f"Error fetching system states: {err}")
         raise HTTPException(status_code=500, detail="Database error")
+    finally:
+        cursor.close()
+        mydb.close()
+
+
+@app.get("/api/csv")
+async def download_system_states(network: str = 'mainnet', epoch: int = None):
+    mydb = pool.get_connection()
+    cursor = mydb.cursor()
+    try:
+        query = f"SELECT * FROM system_state WHERE network = '{network}';"
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        # CSV column headers
+        csv_columns = [
+            "epoch", "network", "sui_address", "protocol_pubkey_bytes", "network_pubkey_bytes",
+            "worker_pubkey_bytes", "name", "description", "image_url", "project_url", "net_address",
+            "p2p_address", "primary_address", "worker_address", "voting_power", "gas_price",
+            "commission_rate", "stake", "apy"
+        ]
+
+        # Create a list to store rows as dictionaries
+        data = []
+        for result in results:
+            system_state_dict = {
+                "epoch": result[0],
+                "network": result[1],
+                "sui_address": result[2],
+                "protocol_pubkey_bytes": result[3],
+                "network_pubkey_bytes": result[4],
+                "worker_pubkey_bytes": result[5],
+                "name": result[6],
+                "description": result[7],
+                "image_url": result[8],
+                "project_url": result[9],
+                "net_address": result[10],
+                "p2p_address": result[11],
+                "primary_address": result[12],
+                "worker_address": result[13],
+                "voting_power": result[14],
+                "gas_price": result[15],
+                "commission_rate": result[16],
+                "stake": result[17],
+                "apy": result[18]
+            }
+            data.append(system_state_dict)
+
+        # Generate the CSV content
+        def generate_csv():
+            yield ",".join(csv_columns) + "\n"
+            for item in data:
+                yield ",".join(str(item[column]) for column in csv_columns) + "\n"
+
+        return StreamingResponse(generate_csv(), media_type="text/csv", headers={"Content-Disposition": 'attachment; filename="system_states.csv"'})
+
+    except mysql.connector.Error as err:
+        logging.error(f"Error fetching system states: {err}")
+        raise HTTPException(status_code=500, detail="Database error")
+
     finally:
         cursor.close()
         mydb.close()
